846f035de65d98c59914234711d6a3037fbe92c9
huihui.zhang <glorydom@163.com>
2016-11-12 23:03:21 +0800

    添加获取本地和公网IP的功能

---
+++ src/main/java/util/NetworkUtil.java
@@ -0,0 +1,89 @@
+package util;
+
+import sun.net.util.IPAddressUtil;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by huihui.b.zhang on 11/12/2016.
+ */
+public class NetworkUtil {
+
+
+
+    public static boolean internalIp(String ip) {
+
+        if("127.0.0.1".equalsIgnoreCase(ip))
+            return true;
+
+        byte[] addr = IPAddressUtil.textToNumericFormatV4(ip);
+        return internalIp(addr);
+    }
+    private static boolean internalIp(byte[] addr) {
+        final byte b0 = addr[0];
+        final byte b1 = addr[1];
+        //10.x.x.x/8
+        final byte SECTION_1 = 0x0A;
+        //172.16.x.x/12
+        final byte SECTION_2 = (byte) 0xAC;
+        final byte SECTION_3 = (byte) 0x10;
+        final byte SECTION_4 = (byte) 0x1F;
+        //192.168.x.x/16
+        final byte SECTION_5 = (byte) 0xC0;
+        final byte SECTION_6 = (byte) 0xA8;
+        switch (b0) {
+            case SECTION_1:
+                return true;
+            case SECTION_2:
+                if (b1 >= SECTION_3 && b1 <= SECTION_4) {
+                    return true;
+                }
+            case SECTION_5:
+                switch (b1) {
+                    case SECTION_6:
+                        return true;
+                }
+            default:
+                return false;
+        }
+    }
+
+    private static List<String> getLocalIPList() {
+        List<String> ipList = new ArrayList<String>();
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface networkInterface;
+            Enumeration<InetAddress> inetAddresses;
+            InetAddress inetAddress;
+            String ip;
+            while (networkInterfaces.hasMoreElements()) {
+                networkInterface = networkInterfaces.nextElement();
+                inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    inetAddress = inetAddresses.nextElement();
+                    if (inetAddress != null && inetAddress instanceof Inet4Address) { // IPV4
+                        ip = inetAddress.getHostAddress();
+                        ipList.add(ip);
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ipList;
+    }
+
+    public static void main(String[] args){
+        List<String> ips = getLocalIPList();
+        for(String ip: ips){
+            System.out.println(ip + " and the ip is public?:" + internalIp(ip));
+        }
+    }
+}
